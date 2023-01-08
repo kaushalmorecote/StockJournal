517e67710420d296080370b8f74ebf0fd93fc8f1
Kaushal Shantaram More <104713521+kaushalmorecote@users.noreply.github.com>
2023-01-08 11:29:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 const CONNECTION_URL =
   "mongodb+srv://kaushalMore:Kunal594@cluster0.juxcepe.mongodb.net/?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 5000;
+//const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 mongoose.set("strictQuery", false);
 
